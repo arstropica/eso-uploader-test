85363c0dec44c14fb95109c4d08d4a71a7edb847
Akin Williams <aowilliams@arstropica.com>
2025-08-22 01:26:31 -0700

    Updated jest config.

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,12 @@ module.exports = {
   coverageProvider: "v8",
   maxConcurrency: 5,
   maxWorkers: 5,
-  roots: ["src"],
-  testMatch: ["**/*.test.js"],
+  roots: ["tests"],
+  moduleFileExtensions: ["js", "ts"],
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+  testRegex: ".*\\.spec\\.ts$",
   testPathIgnorePatterns: ["/node_modules/", "/tests/readiness.playwright.spec.js"],
   testTimeout: 60 * 1000,
 };
